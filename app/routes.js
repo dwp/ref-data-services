ed0a8fe5e649fb4353f87869ad9564c72e6f272c
malkaz-12 <Malka.Zaidi@engineering.digital.dwp.gov.uk>
2026-03-05 09:24:10 +0000

    added all the updated changes
    GitOrigin-RevId: 68765a85cfeba1e7fab6f2430505bac5c5e37347

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,5 @@
 //
+
 // For guidance on how to create routes see:
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
@@ -6,4 +7,33 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+// Apply RDS only to all /v1 pages
+router.all('/v1*', function (req, res, next) {  
+    res.locals.serviceName = 'RDS' 
+    next()
+})
+
 // Add your routes here
+
+
+// Start page
+
+router.get('/v1', function (req, res) {  res.render('v1/index');});
+
+
+// Next page
+router.get('/v1/start-now', function (req, res) {  res.render('v1/start-now');});
+
+router.get('/v1/sign-in-options', function (req, res) {  res.render('v1/sign-in-radio');});``
+
+//sing-in-options
+router.post('/v1/sign-in-options', function (req, res) 
+{  const choice = req.body.signIn;
+     if (choice === 'username') 
+        {    return res.redirect('/v1/sign-in-username');  
+
+        }
+     if (choice === 'sso') {
+        return res.redirect('/v1/sign-in-sso');  
+    }
+});
